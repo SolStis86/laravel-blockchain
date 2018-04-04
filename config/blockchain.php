8ff16c5af30a7cc1a32823076a14ce778aa62e81
jamesf <james@mvdigital.co>
2018-04-04 17:13:43 +0100

    Added config options

--- config/blockchain.php
+++ config/blockchain.php
@@ -1,7 +1,14 @@
 <?php
-
 return [
     'api_key' => env('BLOCKCHAIN_API_KEY', null),
 
     'local_service_port' => env('BLOCKCHAIN_LOCAL_SERVICE_PORT', 3000),
+
+    'wallet_table_name' => 'blockchain_wallets',
+
+    'transaction_table_name' => 'blockchain_transactions',
+
+    'wallet_parent_model' => App\User::class,
+
+    'callback_url_base' => env('APP_URL') . '/blockchain',
 ];
